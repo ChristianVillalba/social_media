c6bb440b8412700b81da2d35051006272d0e5514
Christian Villalba UK <christianvillalba.uk@gmail.com>
2021-05-19 13:47:00 +0100

    create project, add first app, User model created

---
+++ socialsite/accounts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from . import forms
+# Create your views here.
+class SignUp(CreateView):
+    pass
