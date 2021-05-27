ccb1bff43770d7b86e565dc6dcbc961ce2040705
Christian Villalba UK <christianvillalba.uk@gmail.com>
2021-05-27 11:20:10 +0100

    finish models.py (Groups & Posts)

--- socialsite/posts/models.py
+++ socialsite/posts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
+from django.urls import reverse
+import misaka  # if Error: pip install misaka (or: conda install -c conda-forge misaka)
+from groups.models import Group
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    create_at = models.DateTimeField(auto_now=True)
+    message = models.TextField()
+    message_html = models.TextField(editable=False)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, blank=True, related_name="posts")
+    def __str__(self):
+        return self.message
+    def save(self, *args, **kwargs):
+        self.message_html = misaka.html(self.message)
+        super().save(*args,**kwargs)
+    def get_abolute_url(self):
+        return reverse("post:single", kwargs={"username":self.user.username, "pk":self.pk})
+    class Meta:
+        ordering = ["-created_at"]
+        unique_together = ["user","message"]
