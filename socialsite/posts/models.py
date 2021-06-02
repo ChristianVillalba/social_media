df8c4f966d6259241c8fd2fcd556d62ab5b8ab99
Christian Villalba UK <christianvillalba.uk@gmail.com>
2021-06-02 09:11:45 +0100

    debugging urls and redirections: posts app

--- socialsite/posts/models.py
+++ socialsite/posts/models.py
@@ -8,18 +8,19 @@ User = get_user_model()
 
 # Create your models here.
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    user = models.ForeignKey(User,related_name="posts", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now=True)
     message = models.TextField()
     message_html = models.TextField(editable=False)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, blank=True, related_name="posts")
+    group = models.ForeignKey(Group, related_name="posts", null=True, blank=True, on_delete=models.CASCADE)
     def __str__(self):
         return self.message
     def save(self, *args, **kwargs):
         self.message_html = misaka.html(self.message)
-        super().save(*args,**kwargs)
+        super().save(*args , **kwargs)
     def get_absolute_url(self):
-        return reverse("post:single", kwargs={"username":self.user.username, "pk":self.pk})
+        return reverse("posts:single",
+                    kwargs={"username":self.user.username, "pk":self.pk})
     class Meta:
-        ordering = ["created_at"]
-        unique_together = ["user","message"]
+        ordering = ["-created_at"]
+        unique_together = ["user", "message"]
